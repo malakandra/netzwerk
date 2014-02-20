f0319657b91f94c163cc9586988b80099a741c64
Ferdinand Szekeresch <ferdinand.szekeresch@gmail.com>
2014-02-20 18:15:05 +0100

    Fixed greeter
    * Adjusted greeter to changed Authentication API

--- greeter.js
+++ greeter.js
@@ -1,69 +1,59 @@
-var url = "https://netzwerk.firebaseio.com";
+var url = 'https://netzwerk.firebaseio.com';
 
-angular.module("greeter", ["firebase"], function($routeProvider) {
-    $routeProvider.when("/register", {
-        templateUrl: "register.html",
-        controller: RegisterCtrl
-    });
-    $routeProvider.otherwise({
-        templateUrl: "login.html",
-        controller: LoginCtrl
-    });
+var app = angular.module('greeter', [ 'ngRoute', 'firebase' ], function(
+		$routeProvider) {
+	$routeProvider.when('/register', {
+		templateUrl : 'register.html',
+		controller : RegisterCtrl
+	});
+	$routeProvider.otherwise({
+		templateUrl : 'login.html',
+		controller : LoginCtrl
+	});
 });
 
-function GreeterMainViewCtrl($scope, $rootScope, angularFireAuth, angularFire) {
-    var firebase = new Firebase(url);
-    angularFireAuth.initialize(firebase, {
-        scope: $rootScope,
-        name: "user"
-    });
-    $scope.logout = function() {
-        angularFireAuth.logout();
-    }
+function GreeterMainViewCtrl($scope, $location, $rootScope, $firebaseSimpleLogin,
+		$firebase) {
+	var firebase = new Firebase(url);
+	$rootScope.auth = $firebaseSimpleLogin(firebase);
+	$scope.logout = function() {
+		$rootScope.auth.$logout();
+		$location.path('/login');
+	};
 }
 
-function RegisterCtrl($scope, $location, $rootScope, angularFireAuth, angularFire) {
-    var firebase = new Firebase(url);
-    angularFireAuth.initialize(firebase, {
-        scope: $rootScope,
-        name: "user"
-    });
-    $scope.register = function(username, password, confirmpassword) {
-        if (password != confirmpassword) {
-            $scope.errormsg = "Passwörter stimmen nicht überein!";
-        }
-        else {
-            angularFireAuth.createUser(username, password, function(err, user) {
-                if (err) {
-                    $scope.errormsg = "Es ist ein Fehler aufgetreten: " + err;
-                }
-                else if (user) {
-                    $location.path("#/login");
-                }
-            });
-        }
-    }
+function RegisterCtrl($scope, $location, $rootScope, $firebaseSimpleLogin,
+		$firebase) {
+	var firebase = new Firebase(url);
+	$rootScope.auth = $firebaseSimpleLogin(firebase);
+	$scope.register = function(username, password, confirmpassword) {
+		if (password != confirmpassword) {
+			$scope.errormsg = 'Passwörter stimmen nicht überein!';
+		} else {
+			$rootScope.auth.$createUser(username, password, false);
+		}
+		;
+		$rootScope.$on('$firebaseSimpleLogin:error', function(e, err) {
+			$scope.errormsg = "Registrieren fehlgeschlagen!";
+		});
+	};
 }
 
-function LoginCtrl($scope, $location, $rootScope, angularFireAuth, angularFire) {
-    var firebase = new Firebase(url);
-    angularFireAuth.initialize(firebase, {
-        scope: $rootScope,
-        name: "user"
-    });
-    $scope.login = function(username, password) {
-        angularFireAuth.login('password', {
-            email: username,
-            password: password
-        });
-    };
-    $scope.$on("angularFireAuth:error", function(evt, err) {
-        $scope.errormsg = "Login fehlgeschlagen!"
-        $scope.password = null;
-    });
-    $scope.$on("angularFireAuth:login", function(evt) {
-        $scope.errormsg = null;
-        $scope.password = null;
-        $scope.email = null;
-    });
+function LoginCtrl($scope, $location, $rootScope, $firebaseSimpleLogin,
+		$firebase) {
+	var firebase = new Firebase(url);
+	$rootScope.auth = $firebaseSimpleLogin(firebase);
+	$scope.login = function(username, password) {
+		$rootScope.auth.$login('password', {
+			email : username,
+			password : password
+		}).then(function(user) {
+			$scope.errormsg = null;
+			$scope.password = null;
+			$scope.email = null;
+		}, function(evt, err) {
+			$scope.errormsg = 'Login fehlgeschlagen!';
+			$scope.password = null;
+		});
+	};
 }
